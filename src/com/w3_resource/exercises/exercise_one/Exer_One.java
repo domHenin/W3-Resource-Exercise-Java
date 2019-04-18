6b2290232a915a5f202e1734ddd8d9c12ab69325
domHenin <domhenin.517@gmail.com>
2019-04-18 12:45:50 -0400

    exercise one completed

---
+++ src/com/w3_resource/exercises/exercise_one/Exer_One.java
@@ -0,0 +1,15 @@
+package com.w3_resource.exercises.exercise_one;
+
+public class Exer_One {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        System.out.println("Alexandra Abramov");
+    }
+}
+
+
+//TODO:
+//        Write a Java program to print 'Hello' on screen and then print your name on a separate line. Go to the editor
+//        Expected Output :
+//        Hello
+//        Alexandra Abramov
