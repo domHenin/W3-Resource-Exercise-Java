faa43820e1731ed2191e6e37635e384be1a6ec66
domHenin <domhenin.517@gmail.com>
2019-04-21 23:36:48 -0400

    updated code

--- src/com/w3_resource/exercises/exercise_thirteen/AreaSolution.java
+++ src/com/w3_resource/exercises/exercise_thirteen/AreaSolution.java
@@ -1,5 +1,19 @@
 package com.w3_resource.exercises.exercise_thirteen;
 
-public class AreaSolution {
+class AreaSolution extends GivenNumber {
+    public AreaSolution(double width, double height) {
+        super(width, height);
+    }
 
+    public double solve() {
+        double solution = getWidth() * getHeight();
+
+        System.out.printf("Area Solution: %.2f\n",solution);
+
+//        System.out.println(Math.round(getHeight() * getHeight()));
+        return solution;
+    }
 }
+
+
+// Area is 5.6 * 8.5 = 47.60
